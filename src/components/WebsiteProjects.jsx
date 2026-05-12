38033cca678caa4da236cebbb81e3aa114f82f4f
xpensive <xpensivemedia.co@gmail.com>
2026-05-12 17:15:25 +0530

    style: increase slider card width for improved responsiveness on smaller screens

--- src/components/WebsiteProjects.jsx
+++ src/components/WebsiteProjects.jsx
@@ -120,7 +120,7 @@ const WebsiteProjects = () => {
         }
         @media (max-width: 768px) {
           .slider-card {
-            flex: 0 0 78vw;
+            flex: 0 0 80vw;
             max-width: 70vw;
             margin-left: 0;
             margin-right: 0;
